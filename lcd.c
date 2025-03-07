1c45bce13a404a93ce0a454fd83a70768723d0ee
Riley Guidry <rguidry2@horizon.csueastbay.edu>
2025-03-07 12:31:46 -0800

    Added file lcd.c

---
+++ lcd.c
@@ -0,0 +1,138 @@
+//Custom LCD functions for CMPE492 - Spring 2025
+//Derived from HD44780U datasheet
+
+//LCD has 10 pins: {RS, R/W, DB7, DB6, DB5, DB4, DB3, DB2, DB1, DB0}, and an enable bit
+//Performing functions on the LCD requires a specific setup from the signals above
+//Flicking the enable bit will send this instruction into the HD44780U
+
+// PROBLOMATIC FUCTION; many arguments
+void lcd_SetSignals(int rs, int rw, int d7a, int d6a, int d5a, 
+				int d4a, int d7b, int d6b, int d5b, d4b) {
+	//Implemented for 4-bit communication
+	//This means that DB3:DB0 are not used when sending instructions to LCD
+	//So we cannot write the whole byte DB7:DB0 in one go
+	//Instead, DB7:DB4 will send the upper four bits, and then the bottom four bits.
+	//This means the enable bit needs to be flipped twice for each section of the byte
+	//RS and RW remain constant through both iterations
+	RS = rs;
+	RW = rw;
+	DB7 = d7a;
+	DB6 = d6a;
+	DB5 = d5a; 
+	DB4 = d4a;
+	E = 1;
+	_delay_us(1); //pause for information to fully transfer
+	E = 0;
+	DB7 = d7b;
+	DB6 = d6b;
+	DB5 = d5b; 
+	DB4 = d4b;
+	E = 1;
+	_delay_us(50); //pause for instruction to run
+	E = 0;
+	return;
+}
+
+//Our primary interest is to print strings and characters onto the LCD:
+
+void lcd_print(char[] message, int row, int column);
+void lcd_printChar(char character);
+
+//But preliminary and auxiliary functions are necessary:
+
+void lcd_init();
+void lcd_shiftCursor(int RL);
+void lcd_scroll(int RL);
+int lcd_busy();
+void lcd_clear();
+void lcd_setDD(int address);
+
+
+
+void lcd_init() {
+	//function set: {0, 0, 0, 0, 1, DL, N, F, *, *}; * = don't care
+	//DL = 0: 4 bit communication
+	//N = 1: 2 lines on LCD
+	//F = 0: 5 x 8 dot characters
+	lcd_setSignals(0, 0, 0, 0, 1, 0, 1, 0, 0, 0);
+	
+	//entry mode set: {0, 0, 0, 0, 0, 0, 0, 1, I/D, S}
+	//I/D = 1; AC increments after each write command
+	//S = 0; display does not shift
+	lcd_setSignals(0, 0, 0, 0, 0, 0, 0, 1, 1, 0);
+	return;
+}
+
+void lcd_shiftCursor(int RL) {
+	// R/L = 1: shift right; R/L = 0: shift left
+	// {0, 0, 0, 0, 0, 1, S/C, R/L, *, *}; * = don't care
+	// S/C = 1: display shift; S/C = 0: cursor shift
+	lcd_setSignals(0, 0, 0, 0, 0, 1, 0, RL, 0, 0);
+	return;
+}
+
+int lcd_busy() {
+	lcd_setSignals(0, 1, 0, 0, 0, 0, 0, 0, 0, 0);
+	trisBbits[7] = input; //set BF pin to input before reading
+	return (readBbits[7]); //not sure this is allowed
+}
+
+void lcd_clear() {
+	lcd_setSignals(0, 0, 0, 0, 0, 0, 0, 0, 0, 1);
+	return;
+}
+
+void lcd_setDD(int address) {
+	int d6a = (index >> 6) & 0b1;
+	int d5a = (index >> 5) & 0b1;
+	int d4a = (index >> 4) & 0b1;
+	int d7b = (index >> 3) % 0b1;
+	int d6b = (index >> 2) % 0b1;
+	int d5b = (index >> 1) % 0b1;
+	int d4b = (index >> 0) % 0b1;
+	lcd_setSignals(0, 0, 1, d6a, d5a, d4a, d7b, d6b, d5b, d4b);
+
+	return;
+}
+
+//characters can be represented in binary <'1' == 00110001b>, and are explicitly in C
+//We need to print the character onto LCD by putting it's binary representation into the DDRAM
+//So the setup for the LCD to write into DDRAM {1, 0, b0, b1, b2, b3, b4, b5, b6, b7, b8}
+//Setting the DDRAM address tells the LCD to start writing into DDRAM, and not CGRAM
+//Boundaries for LCD row: top[0x00:0x27], bottom[0x40, 0x67]
+//By default, the first index prints the character immediatly to the first cell of the LCD
+
+//So:
+//Break character into binary digits
+//Set up outputs for writeToDDRAM() <1, 0, b0, b1, b2, b3, b4, b5, b6, b7, b8>
+//Set enable bit
+
+void lcd_printChar(char character) { 
+	int d7a = (character >> 7) & 0b1; //high- order bits first
+	int d6a = (character >> 6) & 0b1;
+	int d5a = (character >> 5) & 0b1;
+	int d4a = (character >> 4) & 0b1;
+	int d7b = (character >> 3) & 0b1; //write low-order bits
+	int d6b = (character >> 2) & 0b1;
+	int d5b = (character >> 1) & 0b1;
+	int d4b = (character >> 0) & 0b1; 
+	lcd_setSignals(1, 0, d7a, d6a, d5a, d4a, d7b, d6b, d5b, d4b);
+
+	return;
+}
+
+void lcd_print(char[] string, int row, int column) {
+	//We can assume that the user knows that writing over the lines can be scrolled over to
+	//Write to AC: {0, 0, 1, ADD, ADD, ADD, ADD, ADD, ADD, ADD}
+	
+	int address = ((row - 1) * 0x40) + column; //first term will be 0 if row is 1, so we start at 0x00 before adding columns
+	address = index - 1; //AC increments after writing this instruction
+	
+	lcd_setDD(address);
+	
+	for (int i = 0; i < string.length(); i++) {
+		lcd_PrintChar(string[i]);
+	}
+	
+	return;
+}
