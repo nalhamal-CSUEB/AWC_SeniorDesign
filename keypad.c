d1f92164f817a580132e4ed0c81a9a5bde15b73f
Riley Guidry <rguidry2@horizon.csueastbay.edu>
2025-03-07 12:13:25 -0800

    Added function int modifyTotal(int total, char character)

--- keypad.c
+++ keypad.c
@@ -2,10 +2,15 @@
 
 //read input
 
-char getSymbol(int row, int column) { 
+char kp_getSymbol(int row, int column) { 
 
 	//assuming row and column will be between 0 - 3
 	//scaling for final matrix may be different, double check!
+    
+    //converts a row and column into the corresponding character on the keypad
+    
+    //e.g. hitting the top left button (row 0 column 0 for example)
+    //should return the character '1'
 
 	switch (row) {
 		case 0:
@@ -37,4 +42,33 @@ char getSymbol(int row, int column) {
 				case 3: return 'E'; //enter
 			}
 	}	
+}
+
+int kp_modifyTotal(int total, char input) {
+
+    //takes parameter total and updates it depending on the inputted character
+    //inputted character is scanned by keypad
+    
+    //because it reads as a character, there needs to be a function to track
+    //the total length/amount after each button press
+    
+	switch (input) {
+		case '0':										//if input is a numeric digit
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9': return (total * 10) + (input - 48); 	//shift current digits to left, 
+														//convert input from ascii to digit,
+														//then append digit to total with addition
+
+		case 'D': return (total / 10);					//if digit is deleted, move all digits right,
+														//truncating off removed digit
+
+		default: return total;							//do nothing in case of null or enter
+	}
 }
