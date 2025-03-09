46ba0ee1ac95a33cdeed5b7ee4aa1825926faf8c
Riley Guidry <rguidry2@horizon.csueastbay.edu>
2025-03-09 00:57:01 -0800

    Edited GPIO interfacing syntax, removed LCD commands for clarity

--- keypad.c
+++ keypad.c
@@ -48,35 +48,6 @@ char kp_getSymbol(int row, int column) {
     return 'N';
 }
 
-int kp_modifyTotal(int total, char input) {
-
-    //takes parameter total and updates it depending on the inputted character
-    //inputted character is scanned by keypad
-    
-    //because it reads as a character, there needs to be a function to track
-    //the total length/amount after each button press
-    
-	switch (input) {
-		case '0':										//if input is a numeric digit
-		case '1':
-		case '2':
-		case '3':
-		case '4':
-		case '5':
-		case '6':
-		case '7':
-		case '8':
-		case '9': return (total * 10) + (input - 48); 	//shift current digits to left, 
-														//convert input from ascii to digit,
-														//then append digit to total with addition
-
-		case 'D': return (total / 10);					//if digit is deleted, move all digits right,
-														//truncating off removed digit
-
-		default: return total;							//do nothing in case of null or enter
-	}
-}
-
 char kp_scanForInput() {
     
     //loop across every keypad input port to test for connection
@@ -84,69 +55,55 @@ char kp_scanForInput() {
     //row and column of button press is determined as we loop
     //the corresponding character is returned from getSymbol()
     
-	for (int i = 8; i < 12; i++) { 				//loop across output ports
-
-		/*latBbits[i] = 1; 					//turn each output port on one by one and test for connection
-											//keypad drive pins start at RB8
-											
-		for (int j = 12; j < 16; j++) { 		//loop across receiving pins
-
-			if (readBbits[j] == 1) { 		//keypad-in bits start at RB12
-											//if connection is found 
-				return getSymbol(i, j); 	//find corresponding symbol for button input
-			}	
-		}
-		latBbits[i + 8] = 0; */				//turn on current output port and check next
-	}
+    //keypad drive pins start at RB8
+    //keypad-in bits start at RB12 
+	LATBbits.LATB8 = 1;                 //turn on keypad driving pin		
+    if (PORTBbits.RB12 == 0) {          //check each keypad input for connection
+        return kp_getSymbol(3, 3);
+    } else if (PORTBbits.RB13 == 0) {
+        return kp_getSymbol(2, 3);
+    } else if (PORTBbits.RB14 == 0) {
+        return kp_getSymbol(1, 3);
+    } else if (PORTBbits.RB15 == 0) {
+        return kp_getSymbol(0, 3);
+    }
+    LATBbits.LATB8 = 0;                 //turn off keypad drive
+    
+    LATBbits.LATB9 = 1; 					
+    if (PORTBbits.RB12 == 0) {                                         
+        return kp_getSymbol(3, 2);
+    } else if (PORTBbits.RB13 == 0) {
+        return kp_getSymbol(2, 2);
+    } else if (PORTBbits.RB14 == 0) {
+        return kp_getSymbol(1, 2);
+    } else if (PORTBbits.RB15 == 0) {
+        return kp_getSymbol(0, 2);
+    }
+    LATBbits.LATB9 = 0;
+    
+    LATBbits.LATB10 = 1; 					
+    if (PORTBbits.RB12 == 0) {                                         
+        return kp_getSymbol(3, 1);
+    } else if (PORTBbits.RB13 == 0) {
+        return kp_getSymbol(2, 1);
+    } else if (PORTBbits.RB14 == 0) {
+        return kp_getSymbol(1, 1);
+    } else if (PORTBbits.RB15 == 0) {
+        return kp_getSymbol(0, 1);
+    }
+    LATBbits.LATB10 = 0;
+    
+    LATBbits.LATB11 = 1; 					
+    if (PORTBbits.RB12 == 0) {                                         
+        return kp_getSymbol(3, 1);
+    } else if (PORTBbits.RB13 == 0) {
+        return kp_getSymbol(2, 1);
+    } else if (PORTBbits.RB14 == 0) {
+        return kp_getSymbol(1, 1);
+    } else if (PORTBbits.RB15 == 0) {
+        return kp_getSymbol(0, 1);
+    }
+    LATBbits.LATB11 = 0;
 
 	return 'N'; //if nothing is found
-}
-
-int kp_getBatchSize(char message[], int size) { 
-
-	//will be inside main 
-	//it's purpose is to return the value we are asking for in "message"
-    
-    //scans for keypad input until 'E' or "enter" is pressed
-    //if button pressed is not enter, tracks the character user touched
-    //outputs this character on the LCD screen for user convenience
-    //numerical total is kept track via 
-    //characters can be deleted, and changes are t
-    //tracks the total by converting
-
-	lcd_clear(); 						//clear after booting
-	lcd_print(message, size, 1, 1);			//prompt user to input
-	lcd_setDD(0x40);					//place cursor on second row
-
-	int position = 1;	//for cursor position in LCD row
-	char input = 'N';	//stores user input
-	int total = 0;		//stores total value
-	
-
-	do {
-		input = kp_scanForInput(); 				//check for button input
-												//returns null if no buttons pressed
-												
-		if (input == 'N') { 					//if no valid buttons pressed
-			continue;							//do nothing and rescan		
-		}
-
-		total = kp_modifyTotal(total, input);		//returns total after appending/deleting new input
-		
-		
-		if (input == 'D' && position > 1) { 	//if we are deleting,
-			position = position - 1; 			//go to previous character
-            lcd_setDD(0x40 + position);         //place cursor over character to delete
-            lcd_printChar(' ');                 //print empty character (delete character)
-            lcd_setDD(0x40 + position);         //move cursor back over empty space
-			
-		} else if (input != 'E') {              //if not deleted and not entering,
-            lcd_printChar(input);               //print numerical character
-        }
-
-		//__delay_ms(500); //make our own delay function
-
-	} while (input != 'E');
-	
-	return total; //return entered amount to main
 }
--- keypad.h
+++ keypad.h
@@ -2,8 +2,6 @@
 #define KEYPAD_H
 
 char kp_getSymbol(int row, int column);
-int kp_modifyTotal(int total, char input);
 char kp_scanForInput();
-int kp_getBatchSize(char message[], int size);
 
 #endif
