11f13a6f25751684dcbd63be078f427f09eefd8b
Riley Guidry <rguidry2@horizon.csueastbay.edu>
2025-03-09 01:00:26 -0800

    Removed fuctions outside of main() and setup()

--- main.c
+++ main.c
@@ -1,35 +1,37 @@
 #include <xc.h>
 #include "config.h"
+#include "definitions.h"
+#include "lcd.h"
+#include "keypad.h"
+#include "misc.h"
 
 void setup()
 {
-    TRISEbits.TRISE5 = 0; //pin 1 - debug led RE5
+    //0 == output
+    //1 == input
+    TRISBSET = 0xB00F; //0b1011 xxxx 0000 1111 ('x' = we did not use pin)
+    TRISDSET = 0x7F00; //0b0111 1111 xxxx zzzz ('z' = pin does not exist)
+    TRISESET = 0x00;   //0b0000 00xx
+    
+    LATESET = 0x00;
 }
 
-volatile void ledBlink()
-{
-    for (int i = 0; i < 1000000; i++)
-    {
-        int j = 0;
-        j += 1;
-        _nop();      
-    }
+void testLCD() {
+    lcd_init();
+    lcd_printChar('H');
+    lcd_printChar('i');
+    delay(1000000);
+    lcd_clear();
+    
+    lcd_print("Testing?", 8, 0, 0);
+    delay(1000000);
+    
+    lcd_setDD(0x00 + 7);
+    lcd_printChar('!');
 }
 
 int main (void)
 {
     setup();
-    
-    while (1)
-    {
-        
-        LATEbits.LATE5 = 0;
-        
-        ledBlink();
-        
-        LATEbits.LATE5 = 1;
-        
-        ledBlink();
-    }
-    
+    testLCD();
 }
